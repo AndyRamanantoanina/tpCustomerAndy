43ce93376f83fb2a62d77d7a3427373d55c89645
Ramanantoanina <rmtandy@gmail.com>
2022-03-09 21:32:46 +0300

    Ajout EJB CustomerManager

---
+++ src/main/java/mg/ramanantoanina/tpcustomerapplicationandy/session/CustomerManager.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package mg.ramanantoanina.tpcustomerapplicationandy.session;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import mg.ramanantoanina.tpcustomerapplicationandy.entities.Customer;
+
+
+/**
+ *
+ * @author Ramanantoanina Andy
+ * Gère la persistance des Customers
+ */
+@Stateless
+public class CustomerManager {
+
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+    @Resource
+    private javax.transaction.UserTransaction utx;
+
+    public List<Customer> getAllCustomers() {  
+      Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList(); 
+    }  
+        
+    public Customer update(Customer customer) {
+      return em.merge(customer);  
+    }          
+
+    public void persist(Customer customer) {
+        try {
+            utx.begin();
+            em.persist(customer);
+            utx.commit();
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
